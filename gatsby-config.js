a5b8e8e5df0fe50a636850a89c3d896525de653b
mttwhlly <matt@mattwhalley.com>
2020-09-10 13:03:01 -0400

    fixed vulnerabilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    pathPrefix: `/techproto/v6/MattDEV/SitePages`,
-    siteMetadata: {
-        title: 'Web Policy & Standards',
-        shortName: 'Web Policy & Standards',
-        description: 'The Official Web Style Guide of the Broward County, Florida Government',
-    },
-    plugins: ['@primer/gatsby-theme-doctocat'],
+  pathPrefix: `/techproto/v6/MattDEV/doctest`,
+  assetPrefix: `/Style%20Library/v7/docs`,
+  siteMetadata: {
+    title: 'Web Policy & Standards',
+    shortName: 'Web Policy & Standards',
+    description: 'The Official Web Style Guide of the Broward County, Florida Government'
+  },
+  plugins: [{
+    resolve: '@primer/gatsby-theme-doctocat',
+    options: {
+      repoRootPath: '', //defaults to '.'
+    }
+  }]
 }
